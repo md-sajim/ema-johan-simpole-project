2094b7668eb52e69a239bc672f63e1781a6ee268
sajim <mdabutaliveb@gmail.com>
2022-09-26 06:59:19 +0600

    update cart section

---
+++ src/componant/Card/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Card = ({card}) => {
+    return (
+        <div>
+            <p>Card length: {card.length}</p>
+        </div>
+    );
+};
+
+export default Card;
--- src/componant/Shop/Shop.js
+++ src/componant/Shop/Shop.js
@@ -1,19 +1,20 @@
 import React, { useEffect, useState } from 'react';
+import Card from '../Card/Card';
 import Product from '../Product/Product';
 import "./Shop.css"
 
 const Shop = () => {
     const [card, setCard] = useState([])
-    const hendelProduct = (product)=>{
-       const newCard = [...card,product];
-       setCard(newCard)
+    const hendelProduct = (product) => {
+        const newCard = [...card, product];
+        setCard(newCard)
     }
-    const [products,setProducts] = useState([])
-    useEffect(()=>{
+    const [products, setProducts] = useState([])
+    useEffect(() => {
         fetch("products.json")
-        .then(res => res.json())
-        .then(data =>setProducts(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
     return (
         <div className='shop-container'>
             <div className="shoping_card_container">
@@ -22,9 +23,11 @@ const Shop = () => {
                 }
             </div>
             <div className="shoping_card">
-                <h3>shoping card</h3>
-                <p>Card length: {card.length}</p>
+                <h3>Order Summary</h3>
+                <div className='card-info'>
+                    <Card card={card}></Card>
                 </div>
+            </div>
         </div>
     );
 };
